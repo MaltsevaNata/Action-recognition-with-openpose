bf51ee32564f2ea464d9fca0d3941991029de73b
MaltsevaNata <natalia_maltseva@bk.ru>
2020-04-18 18:50:19 +0200

    PCA done

--- PCA.py
+++ PCA.py
@@ -1,9 +1,9 @@
 # Feature Extraction with PCA
 
 import numpy as np
-
+from numpy.linalg import eig
 from pandas import read_csv
-
+from matplotlib import pyplot as plt
 from sklearn.decomposition import PCA
 import csv
 
@@ -16,23 +16,60 @@ classes = ['noseX',	'noseY',	'neckX',	'neckY',	'RShoulderX',	'RShoulderY',	'RElb
            'RSmallToeY',	'RHeelX',	'RHeelY'
 ]
 names = ['move_scaner', 'tune_angle', 'tune_height', 'turn_on']
-classes_numbers = [10, 3, 2, 3]
-cl = 3
-for number in range(1,classes_numbers[cl]+1):
-    dataframe = read_csv('/home/natalia/Рабочий стол/Train_data/All/{}{}.csv'.format(names[cl],number), names=classes)
-    myFile = open('/home/natalia/Рабочий стол/Train_data/All/PCA/' + '{}{}.csv'.format(names[cl],number), 'w')
-    array = dataframe.values
+classes_numbers = [10, 3, 2, 2]
+myFile = '/home/natalia/Рабочий стол/Train_data/All/PCA/' + 'all.csv'
+newFile = open('/home/natalia/Рабочий стол/Train_data/All/PCA/' + 'myPCA.csv', 'w')
+dataframe = read_csv(myFile, header=None)
 
-    X = array[1:,:]
+array = dataframe.values
 
-    # feature extraction
+X = array[:, :50]
+M = np.mean(X, axis=0) #среднее каждого столбца
+C = X - M #центрировать значения в каждом столбце, вычитая среднее значение столбца.
+C = C.transpose()
+V = np.cov(C.astype(float)) #матрица ковариации
 
-    pca = PCA(n_components=20)
 
-    fit = pca.fit(X)
+values, vectors = eig(V)  #список собственных значений и список собственных векторов.
+#P = np.dot(values, C.T)
+indexes = np.argsort(values)[::-1]
+sorted_values = values[indexes]
+sorted_vectors = vectors[indexes]
+vec = vectors.T[1]
+result_pre = np.dot(C.T, sorted_vectors.T[:, 0:20])
+#result = result_pre.reshape(-1, 1)
 
-    features = fit.transform(X)
+fig, ax = plt.subplots()
 
-    writer = csv.writer(myFile)
-    writer.writerows(features)
+ax.bar(np.arange(0,20), result_pre[380])
+#labels = classes
+#ax.set_xticks(np.arange(len(labels)))
+#ax.set_xticklabels(labels, rotation = 90)
+ax.set_facecolor('seashell')
+fig.set_facecolor('floralwhite')
+writer = csv.writer(newFile)
+writer.writerows(result_pre)
+# Установим логарифмический масштаб по оси X
+#ax.set_yscale ('log')
+
+# Установим логарифмический масштаб по оси Y
+
+plt.show()
+
+
+'''for cl in range(4):
+    for number in range(1,classes_numbers[cl]+1):
+        dataframe = read_csv('/home/natalia/Рабочий стол/Train_data/All/{}{}.csv'.format(names[cl],number), names=classes)
+
+        array = dataframe.values
+
+        X = array[1:,:]
+
+        shape = (np.shape(X)[0], np.shape(X)[1]+1)
+        new_features = np.zeros(shape)
+        new_features = new_features.astype(str)
+        for i in range(len(X)):
+            new_features[i] = np.append(X[i], names[cl])
+        writer = csv.writer(myFile)
+        writer.writerows(new_features)'''
 
